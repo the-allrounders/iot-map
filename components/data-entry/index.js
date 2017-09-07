d2439eba09bf49be74fae3d64cf03f1537f173d9
Bart Langelaan <langelaanbart@gmail.com>
2017-09-07 11:49:42 +0200

    feat(data-entry): animate value if it is a number

--- components/data-entry/index.js
+++ components/data-entry/index.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import AnimatedNumber from 'react-animated-number';
+import PropTypes from 'prop-types';
 
-const DataEntry = ({ entry }) => (
-  <li>
-    <h3>{entry.label}</h3>
-    <ul>
-      <li>key: {entry.key}</li>
-      <li>type: {entry.type}</li>
-      <li>value: {entry.value}</li>
-    </ul>
-  </li>
-);
+const Nr = ({ value, stepPrecision = 0 }) => {
+  if (stepPrecision) {
+    value = parseFloat(value.toFixed(stepPrecision));
+  }
+  return (
+    <AnimatedNumber value={value} stepPrecision={stepPrecision} formatValue={x => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')} />
+  );
+};
+
+Nr.propTypes = {
+  value: PropTypes.number.isRequired,
+  stepPrecision: PropTypes.number,
+};
+
+const DataEntry = ({ entry }) => {
+  const value = (typeof entry.value === 'number') ? <Nr value={entry.value} stepPrecision={2} /> : entry.value;
+  return (
+    <li>
+      <h3>{entry.label}</h3>
+      <ul>
+        <li>key: {entry.key}</li>
+        <li>type: {entry.type}</li>
+        <li>value: {value}</li>
+      </ul>
+    </li>
+  );
+};
 
 export default DataEntry;
