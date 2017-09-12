780c9e3322aa8a1d60dedc62789baaa8c2d788b9
Tim van de Vathorst <tim@vandevathorst.nl>
2017-09-12 10:51:22 +0200

    Add: actuator input field

--- components/device-panel/index.js
+++ components/device-panel/index.js
@@ -1,23 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import Button from 'react-toolbox/lib/button/Button';
+import Input from 'react-toolbox/lib/input/Input';
 import DataEntry from '../data-entry';
 
-const DevicePanel = ({ device }) => {
-  if(!device) {
-    return (
-      <div>
-        <p>Please first select a marker on the map to show its info.</p>
-      </div>
-    );
+const Wrapper = styled.div`
+  padding: 0 20px 20px 20px;
+`;
+
+class DevicePanel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      actuator: props.device.actuator,
+    };
   }
-  const data = device.data.map(entry => <DataEntry entry={entry} key={entry.key} />);
-  return (
-    <div>
-      <h2>{device.name}</h2>
-      <ul>
-        {data}
-      </ul>
-    </div>
-  );
-};
+
+  handleChange(value) {
+    this.setState({ actuator: value });
+  };
+
+  actuator() {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+
+    fetch(`${process.env.SERVER_ADDR || 'https://iot-open-server.herokuapp.com'}/actuator/${this.props.device._id}`, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(this.state),
+    })
+      .then(res => res.json())
+      .then((json) => {
+        this.setState({ actuator: '' });
+      })
+    .catch(console.error);
+  }
+
+  render() {
+    const { device } = this.props;
+
+    if(!device) {
+      return (
+        <div>
+          <p>Please first select a marker on the map to show its info.</p>
+        </div>
+      );
+    }
+
+    const data = device.data.map(entry => <DataEntry entry={entry} key={entry.key} />);
+
+    return (
+      <Wrapper>
+        <h2>{device.name}</h2>
+        <ul>
+          {data}
+        </ul>
+        <div>
+          <Input type='text' label='Actuator' name='actuator' value={this.state.actuator} onChange={(value) => this.handleChange(value)} />
+          <Button icon='play_circle_outline' label='Trigger actuator' onClick={(e) => this.actuator(e)} flat />
+        </div>
+      </Wrapper>
+    );
+  };
+}
 
 export default DevicePanel;
