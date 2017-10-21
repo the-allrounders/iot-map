4b8635a70fa55003fd3fc99fd3899e2230a50eb6
Ian Wensink <ian@burst-digital.com>
2017-10-21 23:17:19 +0200

    feat(sidebar): fix error in Number mapper when value is null

--- components/data-entry/types/number/index.js
+++ components/data-entry/types/number/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import AnimatedNumber from 'react-animated-number';
 
 const Number = ({ entry }) => {
-  let value = entry.value;
+  let value = parseFloat(entry.value || 0);
   if(Number.stepPrecision) {
     value = parseFloat(value.toFixed(Number.stepPrecision));
   }
   return (
-    <AnimatedNumber value={value} stepPrecision={Number.stepPrecision} formatValue={val => val.toFixed(Number.stepPrecision)} />
+    <AnimatedNumber
+      value={value}
+      stepPrecision={Number.stepPrecision}
+      formatValue={val => val.toFixed(Number.stepPrecision)}
+    />
   );
 };
 
